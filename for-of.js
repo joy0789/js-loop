15403bca4c282dc4381426edcd45e463a4272ad2
joy0789 <johirulislamj62@gmail.comm>
2026-05-20 19:52:04 +0600

    practice for loop

---
+++ for-of.js
@@ -0,0 +1,18 @@
+const number = [255,52,674,84,39];
+
+// for basic structure
+// for(){
+
+// }
+
+for(const num of number){
+    console.log(num);
+}
+
+// another example
+
+const fruits = ['orange', 'apple', 'banana', 'jackfruit', 'watermelon']
+
+for(const fruit of fruits){
+    console.log(fruit);
+}
