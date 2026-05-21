7358b117490176cad06146676b7a2612af65da9c
joy0789 <johirulislamj62@gmail.comm>
2026-05-21 20:20:39 +0600

    practice for loop

---
+++ for.js
@@ -0,0 +1,24 @@
+/**
+ * for (    ;   ;   ){
+ * }
+ * 
+ * 
+ * for ( first part ; second part ; third part){
+ *  loop statement
+ * }
+ * 
+ * for (loop variable ; loop condition ; oop variable increment){
+ *  loop statement
+ * }
+ */
+
+// while loop example
+// let num = 0; // loop variable
+// while(num < 5) { // loop condition
+//     console.log(num); // loop statement
+//     num++; // loop variable increment
+// }
+
+for(let num = 0; num < 5; num++){
+    console.log(num);
+}
---
+++ odd.js
@@ -0,0 +1,9 @@
+// for odd number
+for (let i = 1; i<=10; i=i+2){
+    // console.log(i);
+}
+
+// for even number
+for (let i = 2; i<=10; i=i+2){
+    console.log(i);
+}
